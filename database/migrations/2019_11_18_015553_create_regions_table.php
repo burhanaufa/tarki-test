6d0e902a7072075b06f498507aa1e2eec2e2ba68
syahrizafath <syahrizafath@gmail.com>
2019-11-18 09:22:42 +0700

    fix 1 #6

---
+++ database/migrations/2019_11_18_015553_create_regions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('regions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('region_name', 50);
+            $table->string('icon', 50)->nullable();
+            $table->string('url', 50);
+            $table->integer('parent')->nullable();
+            $table->bigInteger('created_by');
+            $table->timestamps();
+            $table->dateTime('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('regions');
+    }
+}
