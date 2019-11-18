f3cd45720b60e8807a2f780986abff75c06dd095
syahrizafath <syahrizafath@gmail.com>
2019-11-18 09:21:38 +0700

    fix 1 #2

---
+++ database/migrations/2019_11_18_011605_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('category_id')->unsigned();
+            $table->string('title', 50);
+            $table->string('headline', 100);
+            $table->text('description');
+            $table->enum('status', ['0', '1', '2'])->default('0')->comment('0=draft, 1=publish', '2=un publish');
+            $table->bigInteger('created_by');
+            $table->timestamps();
+            $table->dateTime('deleted_at');
+
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
