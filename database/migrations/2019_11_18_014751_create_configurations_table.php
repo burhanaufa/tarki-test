af858a153625fd425074426f61a2e88635e86493
syahrizafath <syahrizafath@gmail.com>
2019-11-18 09:22:28 +0700

    fix 1 #5

---
+++ database/migrations/2019_11_18_014751_create_configurations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConfigurationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('configurations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('config_name', 50);
+            $table->text('description');
+            $table->string('image', 50)->nullable();
+            $table->bigInteger('created_by');
+            $table->timestamps();
+            $table->dateTime('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('configurations');
+    }
+}
