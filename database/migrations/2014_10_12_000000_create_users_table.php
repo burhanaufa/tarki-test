b211a524a54ebf1dd88d09d9212b12fa39cda3d7
syahrizafath <syahrizafath@gmail.com>
2019-11-18 09:22:56 +0700

    fix 1 #7

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('username', 50);
+            $table->string('password', 255);
+            $table->string('full_name', 50);
+            $table->enum('status', ['0', '1'])->default(0);
+            $table->bigInteger('created_by');
             $table->timestamps();
+            $table->dateTime('deleted_at');
         });
     }
 
