99fa5cb2afd9002c09ce0b1d091d0826ba2ac10f
syahrizafath <syahrizafath@gmail.com>
2019-11-18 09:21:53 +0700

    fix 1 #3

---
+++ database/migrations/2019_11_18_012549_create_files_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('post_id')->unsigned();
+            $table->string('file_name', 50);
+            $table->enum('file_format', ['img', 'vid', 'doc']);
+            $table->bigInteger('created_by');
+            $table->timestamps();
+            $table->dateTime('deleted_at');
+
+            $table->foreign('post_id')->references('id')->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
