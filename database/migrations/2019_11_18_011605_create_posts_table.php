6fa132cf3d5c02e61b6ba4af228e28a9d29fb987
syahrizafath <syahrizafath@gmail.com>
2019-11-19 16:08:22 +0700

    fix 2 #1

--- database/migrations/2019_11_18_011605_create_posts_table.php
+++ database/migrations/2019_11_18_011605_create_posts_table.php
@@ -19,7 +19,7 @@ class CreatePostsTable extends Migration
             $table->string('title', 50);
             $table->string('headline', 100);
             $table->text('description');
-            $table->enum('status', ['0', '1', '2'])->default('0')->comment('0=draft, 1=publish', '2=un publish');
+            $table->enum('status', ['0', '1', '2'])->default('0')->comment('0=draft, 1=publish, 2=un publish');
             $table->bigInteger('created_by');
             $table->timestamps();
             $table->dateTime('deleted_at');
